c86f8f2066d84ade3516ea2acaa39cd2d7523a2b
hackboxlive <singhsarvshakti127@gmail.com>
2018-01-18 16:53:10 +0530

    Finished

--- main.py
+++ main.py
@@ -13,3 +13,31 @@ NUMBER_OF_THREADS = 4
 
 thread_queue = Queue()
 crawler(DIR_NAME, HOME_URL, DOMAIN_NAME)
+
+def create_crawlers():
+	for _ in range(NUMBER_OF_THREADS):
+		t = threading.Thread(target=work)
+		t.daemon = True
+		t.start()
+
+def work():
+	while True:
+		url = thread_queue.get()
+		crawler.crawl_page(threading.current_thread().name, url)
+		thread_queue.task_done()
+
+def do_work(links):
+	for link in links:
+		thread_queue.put(link)
+	thread_queue.join()
+	start_crawl()
+
+def start_crawl():
+	links = file_to_set(UNCRAWLED_FILE)
+	if len(links) > 0:
+		print(str(len(links)) + ' links still to crawl')
+		do_work(links)
+
+
+create_crawlers()
+start_crawl()
