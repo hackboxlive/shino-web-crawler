105d5751ecfe2c872ade4b5a97ed73446942008f
hackboxlive <singhsarvshakti127@gmail.com>
2018-01-18 15:41:24 +0530

    Added domain.py

---
+++ domain.py
@@ -0,0 +1,15 @@
+from urllib.parse import urlparse
+
+def get_domain(url):
+	try:
+		res = get_subdomain(url).split('.')
+		return res[-2] + '.' + res[-1]
+	except:
+		return ''
+
+
+def get_subdomain(url):
+	try:
+		return urlparse(url).netloc
+	except:
+		return ''
